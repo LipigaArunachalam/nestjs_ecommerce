5f79c5ecce0c648ff62c041af996de0d11fb96e3
DINESH R <ramdinesh2709@gmail.com>
2026-03-03 10:55:00 +0530

    swagger

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { user } from './../schema/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateSellerDto } from './admin.dto';
+import { CreateSellerDto } from './dto/admin.dto';
 import { MailService } from 'src/mail/mail.service';
 import * as bcrypt from 'bcrypt';
 @Injectable()
